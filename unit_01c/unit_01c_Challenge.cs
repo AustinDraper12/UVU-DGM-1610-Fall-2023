ce672841c006ea71c9daccf8b2422e0ea18ae10a
AustinDraper12 <143424822+AustinDraper12@users.noreply.github.com>
2023-09-20 09:33:29 -0600

    Challange 01c

--- unit_01c/unit_01c_Challenge.cs
+++ unit_01c/unit_01c_Challenge.cs
@@ -4,6 +4,48 @@ public class Program
 {
 	public static void Main()
 	{
+		//Challenge 1
+		// It was not working in fiddle becasue it kept changing the number
+		// gets random number
+		Random rng = new Random();
+		int num = rng.Next(1, 11);
+		// sets up how to stop the loop
+		int x = 1;
+		int y = 0;
+		// sets variable for attempts
+		int attempts = 1;
+		while (x > y)
+		{
+			// this was for trouble shooting  Console.WriteLine(num);
+			//asks for number
+			Console.WriteLine("Please Guess Between 1 and 10");
+			int userNum = Convert.ToInt32(Console.ReadLine());
+			//if correct ends loop and congrats
+			if (userNum == num)
+			{
+				Console.WriteLine("Correct Congrats. It took "+ attempts +" attempts");
+				y++;
+			}
+			// num to high
+			else if (userNum > num)
+			{
+				Console.WriteLine("Incorrect sorry number is to high");
+				
+			}
+			//num to low
+			else if (userNum < num)
+			{
+				Console.WriteLine("Incorrect sorry number is to low");
+				
+			}
+			
+			else
+			{
+				Console.WriteLine("This is unacceptable");
+			}
+			//adds attempts
+			attempts++;
+		}
         //Challenge 2
         // askes for food and gets input
 		Console.WriteLine("Please input a your three favorite foods");
