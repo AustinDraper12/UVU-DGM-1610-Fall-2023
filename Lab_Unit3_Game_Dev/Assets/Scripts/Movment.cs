565087e35e0463355b80e59d752c1d1b2bcb3c5f
AustinDraper12 <143424822+AustinDraper12@users.noreply.github.com>
2023-09-29 12:40:04 -0600

    Lab_Unit3

--- Lab_Unit3_Game_Dev/Assets/Scripts/Movment.cs
+++ Lab_Unit3_Game_Dev/Assets/Scripts/Movment.cs
@@ -11,31 +11,45 @@ public class Movment : MonoBehaviour
     private CharacterController controller;
     private Vector3 moveDirection;
     private bool isJumping;
+
+    public bool IsJumping => isJumping;
+
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<CharacterController>();
+        isJumping = true;
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        float translation = Input.GetAxis ("Vertical") * moveSpeed;
-        float straffe = Input.GetAxis ("Horizontal") * moveSpeed;
+        float translation = Input.GetAxis("Vertical") * moveSpeed;
+        float straffe = Input.GetAxis("Horizontal") * moveSpeed;
         translation *= Time.deltaTime;
         straffe *= Time.deltaTime;
-        transform.Translate (straffe, 0, translation);
+        transform.Translate(straffe, 0, translation);
         controller.Move(moveDirection * Time.deltaTime);
-        if (Input.GetKey(KeyCode.Space)) ;
-                if (isJumping == true) ;
-                    controller.Move(moveDirection * Time.deltaTime);
-                    isJumping = false;
-       
-        
-   
-        controller.Move((moveDirection * Time.deltaTime) - gravity);
-       
+        transform.Translate(moveDirection * Time.deltaTime / gravity);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (isJumping == true)
+            {
+                isJumping = false;
+                float jump = moveDirection.y * jumpForce;
+                jump*= Time.deltaTime;
+                transform.Translate(0, jump, 0);
+                isJumping = true;
+
+            }
+            
+            
+        }
+
+
+
 
     }
 }
